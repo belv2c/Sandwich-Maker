d30f90ce240ce53e3f09c544ccfce879f180b167
Brooke Lewis <belv2c@gmail.com>
2017-09-06 17:41:23 -0500

    fixed bread

--- bread.js
+++ bread.js
@@ -2,11 +2,11 @@ console.log("breads");
 // This SandwichMaker IIFE augments the original one
 var SandwichMaker = (function(maker){
 	const breadPrices = {"Wheat": 1.00, "Sourdough": 1.50, "Rye": 1.25, "White": .50};
-
+	console.log(breadPrices);
 
   // Augment the original object with another method
-  maker.addBread = function() {
-    /*return ???;*/
+  maker.addBreadPrices = function(currentBread) {
+    return 
   };
 
   // Return the new, augmented object with the new method on it
